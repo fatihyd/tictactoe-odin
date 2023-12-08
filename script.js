993e51e6bfca1b94ad59a288faf34218d7d4372e
Fatih <yildizafatih@gmail.com>
2023-12-08 16:47:10 +0100

    refactored code and added a restart button

--- script.js
+++ script.js
@@ -1,74 +1,105 @@
-function createBoard() {
-    let boardContainer = document.querySelector("#board-container");
+const Gameboard = (function () {
+    const boardContainer = document.querySelector("#board-container");
     let board = [];
-    for (let i = 0; i < 3; i++) {
-        board[i] = [];
-        for (let j = 0; j < 3; j++) {
-            let cell = document.createElement("button");
-            cell.id = i + "-" + j;
-            board[i][j] = cell;
-            boardContainer.appendChild(board[i][j]);
-        }
-    }
-    return board;
-}
 
-function checkBoard() {
-    /* check horizontal and vertical */
-    for (let i = 0; i < board.length; i++) {
-        /* horizontal */
-        if (board[i][0].textContent !== "") {
-            if (board[i][0].textContent === board[i][1].textContent
-                && board[i][1].textContent === board[i][2].textContent) {
-                winner = board[i][0].textContent;
-                gameInformation.textContent = "Winner: Player " + winner;
+    const createBoard = () => {
+        for (let i = 0; i < 3; i++) {
+            board[i] = [];
+            for (let j = 0; j < 3; j++) {
+                let cell = document.createElement("button");
+                cell.id = i + "-" + j;
+                board[i][j] = cell;
+                boardContainer.appendChild(board[i][j]);
             }
         }
-        /* vertical */
-        if (board[0][i].textContent !== "") {
-            if (board[0][i].textContent === board[1][i].textContent
-                && board[1][i].textContent === board[2][i].textContent) {
-                winner = board[0][i].textContent;
-                gameInformation.textContent = "Winner: Player " + winner;
-            }
-        }
+        return board;
     }
-    /* check the diagonals */
-    if (board[1][1].textContent !== "") {
-        if ((board[0][0].textContent === board[1][1].textContent
-            && board[1][1].textContent === board[2][2].textContent) ||
-            (board[0][2].textContent === board[1][1].textContent
-                && board[1][1].textContent === board[2][0].textContent)) {
-            winner = board[1][1].textContent;
-            gameInformation.textContent = "Winner: Player " + winner;
-        }
-    }
-}
 
-let boardContainer = document.querySelector("#board-container");
-let board = createBoard();
-let isPlayerXTurn = true;
-let winner;
-let gameInformation = document.querySelector("#game-info");
-gameInformation.textContent = isPlayerXTurn ? "Player X's turn" : "Player O's turn";
+    const getBoard = () => board;
 
-for (let i = 0; i < board.length; i++) {
-    for (let j = 0; j < board[i].length; j++) {
-        board[i][j].addEventListener("click", function () {
-            if (!winner) {
-                if (board[i][j].textContent === "") {
-                    if (isPlayerXTurn) {
-                        board[i][j].textContent = "X";
-                        isPlayerXTurn = false;
-                    }
-                    else {
-                        board[i][j].textContent = "O";
-                        isPlayerXTurn = true;
-                    }
-                    gameInformation.textContent = isPlayerXTurn ? "Player X's turn" : "Player O's turn";
-                    checkBoard();
+    board = createBoard();
+
+    return { getBoard };
+})();
+
+function Game() {
+    this.board = Gameboard.getBoard();
+
+    this.isPlayerXTurn = true;
+    this.winner;
+    this.gameInformation = document.querySelector("#game-info");
+    this.gameInformation.textContent = this.isPlayerXTurn ? "Player X's turn" : "Player O's turn";
+
+    this.checkBoard = function () {
+        // check horizontal and vertical
+        for (let i = 0; i < this.board.length; i++) {
+            // horizontal
+            if (this.board[i][0].textContent !== "") {
+                if (this.board[i][0].textContent === this.board[i][1].textContent
+                    && this.board[i][1].textContent === this.board[i][2].textContent) {
+                    this.winner = this.board[i][0].textContent;
+                    this.gameInformation.textContent = "Winner: Player " + this.winner;
                 }
             }
-        });
+            // vertical
+            if (this.board[0][i].textContent !== "") {
+                if (this.board[0][i].textContent === this.board[1][i].textContent
+                    && this.board[1][i].textContent === this.board[2][i].textContent) {
+                    this.winner = this.board[0][i].textContent;
+                    this.gameInformation.textContent = "Winner: Player " + this.winner;
+                }
+            }
+        }
+        // check the diagonals
+        if (this.board[1][1].textContent !== "") {
+            if ((this.board[0][0].textContent === this.board[1][1].textContent
+                && this.board[1][1].textContent === this.board[2][2].textContent) ||
+                (this.board[0][2].textContent === this.board[1][1].textContent
+                    && this.board[1][1].textContent === this.board[2][0].textContent)) {
+                this.winner = this.board[1][1].textContent;
+                this.gameInformation.textContent = "Winner: Player " + this.winner;
+            }
+        }
+    }
+
+    this.handleCellClick = function (i, j) {
+        if (!this.winner) {
+            if (this.board[i][j].textContent === "") {
+                if (this.isPlayerXTurn) {
+                    this.board[i][j].textContent = "X";
+                    this.isPlayerXTurn = false;
+                }
+                else {
+                    this.board[i][j].textContent = "O";
+                    this.isPlayerXTurn = true;
+                }
+                this.gameInformation.textContent = this.isPlayerXTurn ? "Player X's turn" : "Player O's turn";
+                this.checkBoard();
+            }
+        }
+    }
+
+    for (let i = 0; i < this.board.length; i++) {
+        for (let j = 0; j < this.board[i].length; j++) {
+            this.board[i][j].addEventListener("click", () => this.handleCellClick(i, j));
+        }
+    }
+
+    /* restart game */
+    let restartButton = document.querySelector("#restart-game");
+    restartButton.addEventListener("click", () => this.restart());
+    this.restart = function () {
+        this.board = Gameboard.getBoard();
+        this.isPlayerXTurn = true;
+        this.winner = null;
+        this.gameInformation.textContent = this.isPlayerXTurn ? "Player X's turn" : "Player O's turn";
+        for (let i = 0; i < this.board.length; i++) {
+            for (let j = 0; j < this.board[i].length; j++) {
+                this.board[i][j].textContent = "";
+            }
+        }
     }
 }
+
+/* start the game */
+new Game();
